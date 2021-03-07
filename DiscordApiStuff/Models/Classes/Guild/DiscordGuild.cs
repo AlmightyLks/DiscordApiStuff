617aa4520a5b03c773a8d2b2cad2767f7a8008f7
AlmightyLks <AlmightyLks@protonmail.com>
2021-03-07 16:02:44 +0100

    Split up DiscordGuild to partial classes for a better overview

--- DiscordApiStuff/Models/Classes/Guild/DiscordGuild.cs
+++ DiscordApiStuff/Models/Classes/Guild/DiscordGuild.cs
@@ -3,19 +3,12 @@ using DiscordApiStuff.Core.Caching;
 using DiscordApiStuff.Models.Classes.Channel;
 using DiscordApiStuff.Models.Enums;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
 namespace DiscordApiStuff.Models.Classes.Guild
 {
-    public sealed class DiscordGuild : Snowflake
+    public sealed partial class DiscordGuild : Snowflake
     {
-        [JsonIgnore]
-        internal AppendOnlyFixedCache<GuildChannel> ChannelCache;
-
         [JsonPropertyName("name")]
         public string Name { get; set; }
 
@@ -31,36 +24,14 @@ namespace DiscordApiStuff.Models.Classes.Guild
         [JsonPropertyName("discovery_splash")]
         public string DiscoverySplash { get; set; }
 
-        [JsonPropertyName("features")]
-        public List<string> Features { get; set; }
-
-        [JsonPropertyName("emojis")]
-        public List<Emoji> Emojis { get; set; }
-
         [JsonPropertyName("banner")]
         public string Banner { get; set; }
-
-        [JsonPropertyName("owner_id")]
-        [JsonConverter(typeof(SnowflakeConverter))]
-        public ulong OwnerId { get; set; }
-
-        [JsonPropertyName("application_id")]
-        [JsonConverter(typeof(SnowflakeConverter))]
-        public ulong? ApplicationId { get; set; }
 
         [JsonPropertyName("region")]
         public string Region { get; set; }
 
-        [JsonPropertyName("afk_channel_id")]
-        [JsonConverter(typeof(SnowflakeConverter))]
-        public ulong AfkChannelId { get; set; }
-
         [JsonPropertyName("afk_timeout")]
         public int AfkTimeout { get; set; }
-
-        [JsonPropertyName("system_channel_id")]
-        [JsonConverter(typeof(SnowflakeConverter))]
-        public ulong? SystemChannelId { get; set; }
 
         [JsonPropertyName("joined_at")]
         public DateTime? JoinedAt { get; set; }
@@ -74,24 +45,8 @@ namespace DiscordApiStuff.Models.Classes.Guild
         [JsonPropertyName("widget_enabled")]
         public bool? WidgetEnabled { get; set; }
 
-        [JsonPropertyName("widget_channel_id")]
-        [JsonConverter(typeof(SnowflakeConverter))]
-        public ulong? WidgetChannelId { get; set; }
-
-        [JsonPropertyName("verification_level")]
-        public VerificationLevel VerificationLevel { get; set; }
-
-        [JsonPropertyName("roles")]
-        public List<Role> Roles { get; set; }
-
         [JsonPropertyName("default_message_notifications")]
         public int DefaultMessageNotifications { get; set; }
-
-        [JsonPropertyName("mfa_level")]
-        public MfaLevel MfaLevel { get; set; }
-
-        [JsonPropertyName("explicit_content_filter")]
-        public ContentFilterLevel ExplicitContentFilter { get; set; }
 
         [JsonPropertyName("max_presences")]
         public int? MaxPresences { get; set; }
@@ -102,9 +57,6 @@ namespace DiscordApiStuff.Models.Classes.Guild
         [JsonPropertyName("vanity_url_code")]
         public string VanityUrlCode { get; set; }
 
-        [JsonPropertyName("premium_tier")]
-        public PremiumTier PremiumTier { get; set; }
-
         [JsonPropertyName("premium_subscription_count")]
         public int PremiumSubscriptionCount { get; set; }
 
@@ -114,16 +66,35 @@ namespace DiscordApiStuff.Models.Classes.Guild
         [JsonPropertyName("preferred_locale")]
         public string PreferredLocale { get; set; }
 
-        [JsonPropertyName("rules_channel_id")]
-        [JsonConverter(typeof(SnowflakeConverter))]
-        public ulong RulesChannelId { get; set; }
-
-        [JsonPropertyName("public_updates_channel_id")]
-        [JsonConverter(typeof(SnowflakeConverter))]
-        public ulong? PublicUpdatesChannelId { get; set; }
-
         [JsonPropertyName("member_count")]
         public int? MemberCount { get; set; }
+    }
+
+    public sealed partial class DiscordGuild : Snowflake
+    {
+        [JsonPropertyName("premium_tier")]
+        public PremiumTier PremiumTier { get; set; }
+
+        [JsonPropertyName("verification_level")]
+        public VerificationLevel VerificationLevel { get; set; }
+
+        [JsonPropertyName("mfa_level")]
+        public MfaLevel MfaLevel { get; set; }
+
+        [JsonPropertyName("explicit_content_filter")]
+        public ContentFilterLevel ExplicitContentFilter { get; set; }
+
+    }
+    public sealed partial class DiscordGuild : Snowflake
+    {
+        [JsonPropertyName("features")]
+        public string[] Features { get; set; }
+
+        [JsonPropertyName("emojis")]
+        public Emoji[] Emojis { get; set; }
+
+        [JsonPropertyName("roles")]
+        public Role[] Roles { get; set; }
 
         [JsonPropertyName("voice_states")]
         public VoiceState[] VoiceStates { get; set; }
@@ -136,5 +107,35 @@ namespace DiscordApiStuff.Models.Classes.Guild
 
         [JsonPropertyName("presences")]
         public Presence[] Presences { get; set; }
+    }
+    public sealed partial class DiscordGuild : Snowflake
+    {
+        [JsonPropertyName("owner_id")]
+        [JsonConverter(typeof(SnowflakeConverter))]
+        public ulong OwnerId { get; set; }
+
+        [JsonPropertyName("application_id")]
+        [JsonConverter(typeof(SnowflakeConverter))]
+        public ulong? ApplicationId { get; set; }
+
+        [JsonPropertyName("afk_channel_id")]
+        [JsonConverter(typeof(SnowflakeConverter))]
+        public ulong AfkChannelId { get; set; }
+
+        [JsonPropertyName("system_channel_id")]
+        [JsonConverter(typeof(SnowflakeConverter))]
+        public ulong? SystemChannelId { get; set; }
+
+        [JsonPropertyName("widget_channel_id")]
+        [JsonConverter(typeof(SnowflakeConverter))]
+        public ulong? WidgetChannelId { get; set; }
+
+        [JsonPropertyName("rules_channel_id")]
+        [JsonConverter(typeof(SnowflakeConverter))]
+        public ulong RulesChannelId { get; set; }
+
+        [JsonPropertyName("public_updates_channel_id")]
+        [JsonConverter(typeof(SnowflakeConverter))]
+        public ulong? PublicUpdatesChannelId { get; set; }
     }
 }
